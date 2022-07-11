058b6f70a873788823187c4c0fa52757efd81d18
Alex Perez <apramos89@gmail.com>
2022-07-11 15:35:47 +0200

    Create a basic structure to read a file or read a promt line and, based on it, create a new Scanner Struct

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+#![allow(dead_code)]
+
+mod scanner;
+
+use anyhow::{Context, Ok, Result};
+use scanner::Scanner;
+use std::env;
+use std::fs;
+use std::io;
+
+fn run(input: &str) {
+    let scanner = Scanner::new(input);
+    let tokens = scanner.tokens();
+
+    for token in tokens {
+        println!("{}", token);
+    }
+}
+
+fn run_file(filename: &str) -> Result<()> {
+    let content = fs::read_to_string(filename)
+        .with_context(|| format!("Failed to read file from {}", filename))?;
+
+    println!("{}", content);
+    Ok(())
+}
+
+fn run_prompt() -> Result<()> {
+    loop {
+        println!("> ");
+        let mut raw_input = String::new();
+
+        io::stdin()
+            .read_line(&mut raw_input)
+            .expect("Failed to read line");
+
+        let input = raw_input.trim();
+        if input.is_empty() {
+            continue;
+        }
+        run(input);
+    }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        i if i >= 2 => panic!("Usage: cargo run <filename>"),
+        i if i == 2 => run_file(&args[1])?,
+        _ => todo!("run prompt command"),
+    };
+
+    Ok(())
+}
---
+++ src/scanner/mod.rs
@@ -0,0 +1,11 @@
+pub struct Scanner {}
+
+impl Scanner {
+    pub fn new(input: &str) -> Scanner {
+        Scanner {}
+    }
+
+    pub fn tokens(&self) -> Vec<u32> {
+        vec![0; 5]
+    }
+}
